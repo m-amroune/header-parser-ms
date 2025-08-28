f3ea33878862ffa992362eaa5803bcfe385dfd4b
dev-front33 <31637994-dev-front33@users.noreply.replit.com>
2025-08-28 14:50:06 +0000

    feat : add api whoami route to retrieve ip, language and software

--- index.js
+++ index.js
@@ -24,6 +24,14 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', (req, res) => {
+  res.json({
+    ipaddress: req.ip,
+    language: req.get('Accept-Language'),
+    software: req.get('User-Agent')
+  });
+}); // 👈 cette ligne manquait : elle ferme la route proprement
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
